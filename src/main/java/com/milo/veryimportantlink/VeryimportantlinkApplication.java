2b406602607d827434a23837bbba0329c803b642
sosnowskimilosz <sosnowskimilosz@gmail.com>
2022-05-13 15:47:30 +0200

    initial commit with files

---
+++ src/main/java/com/milo/veryimportantlink/VeryimportantlinkApplication.java
@@ -0,0 +1,13 @@
+package com.milo.veryimportantlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryimportantlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryimportantlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milo/veryimportantlink/VeryimportantlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.veryimportantlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeryimportantlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
